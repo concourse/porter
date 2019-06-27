aaa2e2766a8bb9eb3b7df4daa8de6e6e1103c0b4
kcmannem <kmannem@pivotal.io>
2019-06-26 22:56:29 -0400

    testing another approach to watching pods for outputs

--- outputpusher/main.go
+++ outputpusher/main.go
@@ -1,16 +1,16 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"os"
 	"time"
-	"context"
 
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 	"code.cloudfoundry.org/lager"
 	"github.com/concourse/porter/k8s"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
@@ -26,46 +26,93 @@ func main() {
 
 	flag.Parse()
 
+	logger := lager.NewLogger("output-push")
+	logger.Info("")
 	fmt.Println("running your watcher cmd with args", *podname, *containername)
 
 	// creates the in-cluster config
 	config, err := rest.InClusterConfig()
 	if err != nil {
-		panic(err.Error())
+		logger.Error("failed to retrieve cluster config", err)
+		os.Exit(1)
 	}
 	// creates the clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		logger.Error("failed to create client with fetched config", err)
+		os.Exit(1)
 	}
 
-	max_retries := 20
-	for i := 1; i <= max_retries; i++ {
+	// another approach? seems more native with the Watch() client endpoint
+	watch, err := clientset.CoreV1().Pods("default").Watch(metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("metadata.name=%s", *podname),
+	})
+	if err != nil {
+		logger.Error("failed to find pod", err)
+		os.Exit(1)
+	}
+
+	go func() {
+		for event := range watch.ResultChan() {
+			// we only care when event.Type == MODIFIED, This event
+			// occurs when ContainerStatus is updated
+			pod, ok := event.Object.(*v1.Pod)
+			if !ok {
+				log.Fatal("unexpected type")
+			}
+
+			for _, containerInfo := range pod.Status.ContainerStatuses {
+				terminationInfo := containerInfo.State.Terminated
+
+				if containerInfo.Name == *containername && terminationInfo.Terminated != nil {
+
+					if terminationInfo.ExitCode == 0 {
+						url := os.Getenv("BUCKET_URL")
+						bucketConfig := k8s.BucketConfig{
+							Type:   *bucketType,
+							URL:    url,
+							Secret: "notasecret",
+						}
+
+						k8s.Push(logger, context.Background(), bucketConfig, *sourceKey, *destionationPath)
+
+						os.Exit(0)
+					} else if terminationInfo.ExitCode != 0 {
+						logger.Info("output producer container returned non-zero exit code")
+						os.Exit(1)
+					}
+				}
+			}
+		}
+	}()
+
+	// simple approach and works.
+	for {
 		pod, err := clientset.CoreV1().Pods("default").Get(*podname, metav1.GetOptions{})
 		if err != nil {
-			panic(err.Error())
+			logger.Error("failed to find pod", err)
+			os.Exit(1)
 		}
+
 		for _, c := range pod.Status.ContainerStatuses {
-			fmt.Println("searching containers", c.Name)
-			if c.Name == *containername {
-				fmt.Println("looping on containers", c.Name)
-				if c.State.Terminated != nil && c.State.Terminated.ExitCode == 0 {
-					fmt.Println("ready to upload output")
+			if c.Name == *containername && c.State.Terminated != nil {
+				if c.State.Terminated.ExitCode == 0 {
 					url := os.Getenv("BUCKET_URL")
 					bucketConfig := k8s.BucketConfig{
-						Type: *bucketType,
-						URL: url,
+						Type:   *bucketType,
+						URL:    url,
 						Secret: "notasecret",
 					}
-					k8s.Push(lager.NewLogger("output_pusher"), context.Background(), bucketConfig, *sourceKey, *destionationPath)
+
+					k8s.Push(logger, context.Background(), bucketConfig, *sourceKey, *destionationPath)
+
 					os.Exit(0)
-				} else if c.State.Terminated != nil && c.State.Terminated.ExitCode != 0 {
-					fmt.Println("container step failed with exit code", c.State.Terminated.ExitCode)
-					os.Exit(5)
+				} else if c.State.Terminated.ExitCode != 0 {
+					logger.Info("output producer container returned non-zero exit code")
+					os.Exit(1)
 				}
 			}
 		}
 		time.Sleep(2 * time.Second)
 	}
 }
-
