cf095f02f0075ae61d07676ff8fbb60178d68aa4
Nader Ziada <nziada@pivotal.io>
2019-07-22 11:11:40 -0400

    [#4054] Refactor watch
    - renamed Start -> Wait
    - refactored Wait to be simpler and inline
    - added more tests
    
    Signed-off-by: Sameer Vohra <svohra@pivotal.io>

--- outputs/watch/watch.go
+++ outputs/watch/watch.go
@@ -12,7 +12,9 @@ import (
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
-var ErrMonitoredContainerExitFail = errors.New("monitored container did not exit well")
+var ErrMonitoredContainerExitFail = errors.New("monitored container exited with error")
+var ErrContainerNotFoundInPod = errors.New("container doesn't exist under specified Pod")
+var ErrWatcherclosedWithoutTerminationStatus = errors.New("Watcher result channel closed without container being terminated")
 
 type ContainerWatcher struct {
 	Client        kubernetes.Interface
@@ -20,15 +22,16 @@ type ContainerWatcher struct {
 	PodName       string
 }
 
-func (cw *ContainerWatcher) Start(logger lager.Logger) error {
+func (cw *ContainerWatcher) Wait(logger lager.Logger) error {
 	watch, err := cw.Client.CoreV1().Pods("default").Watch(metav1.ListOptions{
 		FieldSelector: fmt.Sprintf("metadata.name=%s", cw.PodName),
 	})
+	defer watch.Stop()
+
 	if err != nil {
 		logger.Error("failed to find pod", err)
 		return err
 	}
-
 	for event := range watch.ResultChan() {
 		// We only care when event.Type == MODIFIED
 		// MODIFIED events occur when ContainerStatus is
@@ -40,46 +43,38 @@ func (cw *ContainerWatcher) Start(logger lager.Logger) error {
 			continue
 		}
 
-		err := CheckTermination(
-			cw.ContainerName,
-			pod.Status.ContainerStatuses,
-			func() error {
-				logger.Info("monitored container exited successfully", lager.Data{})
-				// *push outputs here*
-				return nil
-			},
-			func(exitCode int32) error {
-				logger.Info("monitored container returned non-zero exit code", lager.Data{
-					"podname":  cw.PodName,
-					"ExitCode": exitCode,
-				})
-				return ErrMonitoredContainerExitFail
-			},
-		)
-
-		if err != nil {
-			return err
+		if isContainerInPod(pod, cw) != true {
+			return ErrContainerNotFoundInPod
 		}
-	}
-	return nil
-}
 
-func CheckTermination(
-	container string,
-	statuses []v1.ContainerStatus,
-	onExit0 func() error,
-	onExitN func(exitCode int32) error,
-) error {
-	for _, containerInfo := range statuses {
-		state := containerInfo.State
+		for _, containerInfo := range pod.Status.ContainerStatuses {
+			state := containerInfo.State
 
-		if containerInfo.Name == container && state.Terminated != nil {
-			if state.Terminated.ExitCode == 0 {
-				return onExit0()
-			} else if state.Terminated.ExitCode != 0 {
-				return onExitN(state.Terminated.ExitCode)
+			if containerInfo.Name == cw.ContainerName && state.Terminated != nil {
+				if state.Terminated.ExitCode == 0 {
+					logger.Info("monitored container exited successfully", lager.Data{})
+					return nil
+				} else if state.Terminated.ExitCode != 0 {
+					logger.Info("monitored container returned non-zero exit code", lager.Data{
+						"podname":  cw.PodName,
+						"ExitCode": state.Terminated.ExitCode,
+					})
+					return ErrMonitoredContainerExitFail
+				}
 			}
 		}
 	}
-	return nil
+	return ErrWatcherclosedWithoutTerminationStatus
 }
+
+func isContainerInPod(pod *v1.Pod, cw *ContainerWatcher) bool {
+	found := false
+	for _, container := range pod.Spec.Containers {
+		if container.Name == cw.ContainerName {
+			found = true
+		}
+	}
+	return found
+}
+
+
--- outputs/watch/watch_test.go
+++ outputs/watch/watch_test.go
@@ -1,206 +1,107 @@
-package watch_test
+package watch
 
 import (
-	cwatch "github.com/concourse/porter/outputs/watch"
+	"code.cloudfoundry.org/lager/lagertest"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/testing"
+)
+
+var (
+	logger *lagertest.TestLogger
 )
 
 var _ = Describe("Watch", func() {
+	var (
+	fakeClient *fake.Clientset
+	watcher *watch.FakeWatcher
+	pod *v1.Pod
+	updatedPod *v1.Pod
+	containerWatcher *ContainerWatcher
+	)
 
-	Context("checkTermination", func() {
-		It("returns nil and does nothing when termination is NOT found", func() {
-			statuses := []v1.ContainerStatus{
-				v1.ContainerStatus{
-					Name: "neighboring-container",
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode: 0,
-							Reason:   "for a test",
+	BeforeEach(func(){
+		logger = lagertest.NewTestLogger("test")
+		fakeClient = fake.NewSimpleClientset()
+		watcher = watch.NewFake()
+
+		fakeClient.PrependWatchReactor("pods", testing.DefaultWatchReactor(watcher, nil))
+
+		pod = &v1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: "default",
+				Name:      "test-task-pod",
+			},
+			Spec: v1.PodSpec{
+				NodeName: "node1",
+				Containers: []v1.Container{
+					{
+						Name: "task-container",
+					},
+					{
+						Name: "neighboring-container",
+					},
+				},
+			},
+
+			Status: v1.PodStatus{
+				ContainerStatuses: []v1.ContainerStatus{
+					{
+						Name: "task-container",
+						State: v1.ContainerState{
+							Waiting: &v1.ContainerStateWaiting{"some-reason", "for a test"},
+						},
+					},
+					{
+						Name: "neighboring-container",
+						State: v1.ContainerState{
+							Running: &v1.ContainerStateRunning{},
 						},
 					},
 				},
-				v1.ContainerStatus{
-					Name: "task-container",
-					State: v1.ContainerState{
-						Waiting: &v1.ContainerStateWaiting{"some-reason", "for a test"},
-					},
-				},
-				v1.ContainerStatus{
-					Name: "neighboring-container",
-					State: v1.ContainerState{
-						Running: &v1.ContainerStateRunning{},
-					},
-				},
-			}
-
-			onExit0CallCount := 0
-			onExitNCallCount := 0
-
-			err := cwatch.CheckTermination(
-				"task-container",
-				statuses,
-				func() error {
-					onExit0CallCount++
-					return nil
-				},
-				func(exit int32) error {
-					onExitNCallCount++
-					return nil
-				},
-			)
-
-			Expect(err).ToNot(HaveOccurred())
-			Expect(onExit0CallCount).To(Equal(0))
-			Expect(onExitNCallCount).To(Equal(0))
-		})
-
-		It("returns nil and invokes onExit0 func when successful termination is found", func() {
-			statuses := []v1.ContainerStatus{
-				v1.ContainerStatus{
-					Name: "neighboring-container",
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode: 0,
-							Reason:   "for a test",
-						},
-					},
-				},
-				v1.ContainerStatus{
-					Name: "task-container",
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode: 0,
-							Reason:   "for a test",
-						},
-					},
-				},
-			}
-
-			onExit0CallCount := 0
-			onExitNCallCount := 0
-
-			err := cwatch.CheckTermination(
-				"task-container",
-				statuses,
-				func() error {
-					onExit0CallCount++
-					return nil
-				},
-				func(exit int32) error {
-					onExitNCallCount++
-					return nil
-				},
-			)
-
-			Expect(err).ToNot(HaveOccurred())
-			Expect(onExit0CallCount).To(Equal(1))
-			Expect(onExitNCallCount).To(Equal(0))
-
-		})
-
-		It("returns error and invokes onExitN func when unsuccessful termination is found", func() {
-			statuses := []v1.ContainerStatus{
-				v1.ContainerStatus{
-					Name: "neighboring-container",
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode: 0,
-							Reason:   "for a test",
-						},
-					},
-				},
-				v1.ContainerStatus{
-					Name: "task-container",
-					State: v1.ContainerState{
-						Terminated: &v1.ContainerStateTerminated{
-							ExitCode: 2,
-							Reason:   "for a test",
-						},
-					},
-				},
-			}
-
-			onExit0CallCount := 0
-			onExitNCallCount := 0
-
-			err := cwatch.CheckTermination(
-				"task-container",
-				statuses,
-				func() error {
-					onExit0CallCount++
-					return nil
-				},
-				func(exit int32) error {
-					onExitNCallCount++
-					return cwatch.ErrMonitoredContainerExitFail
-				},
-			)
-
-			Expect(err).To(HaveOccurred())
-			Expect(err).To(Equal(cwatch.ErrMonitoredContainerExitFail))
-			Expect(onExit0CallCount).To(Equal(0))
-			Expect(onExitNCallCount).To(Equal(1))
-		})
-
+			},
+		}
 	})
-	/*
-		var (
-			logger *lagertest.TestLogger
-		)
 
-		BeforeEach(func() {
-			logger = lagertest.NewTestLogger("test")
-		})
-		Context("waits for watch to finish waiting", func() {
 
-			Context("pushes the output if the termination exitCode is 0", func() {
-				fakeClient := fake.NewSimpleClientset()
-				watcher := watch.NewFake()
-				fakeClient.PrependWatchReactor("pods", ktesting.DefaultWatchReactor(watcher, nil))
+		Context("when Container.State is TERMINATED", func() {
+			BeforeEach(func() {
 
-				pod1 := &v1.Pod{
+				updatedPod = &v1.Pod{
 					ObjectMeta: metav1.ObjectMeta{
 						Namespace: "default",
 						Name:      "test-task-pod",
 					},
 					Spec: v1.PodSpec{
 						NodeName: "node1",
-					},
-					Status: v1.PodStatus{
-						ContainerStatuses: []v1.ContainerStatus{
-							v1.ContainerStatus{
+						Containers: []v1.Container{
+							{
 								Name: "task-container",
-								State: v1.ContainerState{
-									Waiting: &v1.ContainerStateWaiting{"some-reason", "for a test"},
-								},
 							},
-							v1.ContainerStatus{
+							{
 								Name: "neighboring-container",
-								State: v1.ContainerState{
-									Running: &v1.ContainerStateRunning{},
-								},
 							},
 						},
 					},
 				}
 
-				// simulate add/update/delete watch events
-				watcher.Add(pod1)
-				// assert Watch did not terminate
+				containerWatcher = &ContainerWatcher{
+					Client:        fakeClient,
+					ContainerName: "task-container",
+					PodName:       "test-task-pod",
+				}
+			})
+			It("and exitCode is Zero, Watch returns nil error", func() {
+				go func () {
+					watcher.Add(pod)
 
-				samePod1ButUpdated := &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: "default",
-						Name:      "test-task-pod",
-					},
-					Spec: v1.PodSpec{
-						NodeName: "node1",
-					},
-					Status: v1.PodStatus{
+					updatedPodStatus := v1.PodStatus{
 						ContainerStatuses: []v1.ContainerStatus{
-							v1.ContainerStatus{
+							{
 								Name: "task-container",
 								State: v1.ContainerState{
 									Terminated: &v1.ContainerStateTerminated{
@@ -209,70 +110,32 @@ var _ = Describe("Watch", func() {
 									},
 								},
 							},
-							v1.ContainerStatus{
+							{
 								Name: "neighboring-container",
 								State: v1.ContainerState{
 									Running: &v1.ContainerStateRunning{},
 								},
 							},
 						},
-					},
-				}
-				watcher.Modify(samePod1ButUpdated)
-				// assert Watch terminated
+					}
+					updatedPod.Status = updatedPodStatus
+
+					watcher.Modify(updatedPod)
+
+				}()
+
+				err := containerWatcher.Wait(logger)
+				Expect(err).ToNot(HaveOccurred())
 			})
 
-			Context("does not push outputs if the termination exitCode is non-zero", func() {
-				fakeClient := fake.NewSimpleClientset()
-				watcher := watch.NewFake()
-				fakeClient.PrependWatchReactor("pods", ktesting.DefaultWatchReactor(watcher, nil))
+			It("exitCode is NOT Zero, Watch returns an error", func() {
 
-				pod1 := &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: "default",
-						Name:      "test-task-pod",
-					},
-					Spec: v1.PodSpec{
-						NodeName: "node1",
-					},
-					Status: v1.PodStatus{
+				go func () {
+					watcher.Add(pod)
+
+					updatedPod.Status = v1.PodStatus{
 						ContainerStatuses: []v1.ContainerStatus{
-							v1.ContainerStatus{
-								Name: "task-container",
-								State: v1.ContainerState{
-									Waiting: &v1.ContainerStateWaiting{"some-reason", "for a test"},
-								},
-							},
-							v1.ContainerStatus{
-								Name: "neighboring-container",
-								State: v1.ContainerState{
-									Running: &v1.ContainerStateRunning{},
-								},
-							},
-						},
-					},
-				}
-				containerWatcher := &cwatch.ContainerWatcher{
-					Client:        fakeClient,
-					ContainerName: "task-container",
-					PodName:       "test-task-pod",
-				}
-
-				// simulate add/update/delete watch events
-				watcher.Add(pod1)
-				// assert Watch did not terminate
-
-				samePod1ButUpdated := &v1.Pod{
-					ObjectMeta: metav1.ObjectMeta{
-						Namespace: "default",
-						Name:      "test-task-pod",
-					},
-					Spec: v1.PodSpec{
-						NodeName: "node1",
-					},
-					Status: v1.PodStatus{
-						ContainerStatuses: []v1.ContainerStatus{
-							v1.ContainerStatus{
+							{
 								Name: "task-container",
 								State: v1.ContainerState{
 									Terminated: &v1.ContainerStateTerminated{
@@ -281,19 +144,42 @@ var _ = Describe("Watch", func() {
 									},
 								},
 							},
-							v1.ContainerStatus{
+							{
 								Name: "neighboring-container",
 								State: v1.ContainerState{
 									Running: &v1.ContainerStateRunning{},
 								},
 							},
 						},
-					},
-				}
-				watcher.Add(samePod1ButUpdated)
-				Expect(containerWatcher.Start(logger))
-				// assert Watch terminated
+					}
+
+					watcher.Add(updatedPod)
+				}()
+
+
+				err := containerWatcher.Wait(logger)
+				Expect(err).To(Equal(ErrMonitoredContainerExitFail))
 			})
 		})
-	*/
+
+		Context("when Container name is not found in the Pod", func() {
+		BeforeEach(func() {
+			containerWatcher = &ContainerWatcher{
+				Client:        fakeClient,
+				ContainerName: "some-non-existent-task-container",
+				PodName:       "test-task-pod",
+			}
+		})
+		It("Watch returns an error", func() {
+			go func () {
+				watcher.Add(pod)
+			}()
+
+			err := containerWatcher.Wait(logger)
+			Expect(err).To(Equal(ErrContainerNotFoundInPod))
+		})
+
+
+	})
+
 })
